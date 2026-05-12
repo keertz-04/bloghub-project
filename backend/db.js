1014aaf714c352a1765dd54e068326425ea0927c
Keerthiga <keerthizaku@gmail@gmail.com>
2026-05-12 21:21:44 +0530

    frontend fixed

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,12 @@
-const mongoose=require('mongoose');
-const connectDB=async()=>{};
-module.exports=connectDB;
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
