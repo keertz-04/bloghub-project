b6a81a361e565d2c5698e2a3252d6557f9f2abe7
Keerthiga <keerthizaku@gmail@gmail.com>
2026-05-12 21:21:44 +0530

    frontend fixed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
-const express=require('express');
-const cors=require('cors');
-const app=express();
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+
+const adminRoutes = require("./routes/adminRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+
+const app = express();
+connectDB();
+
 app.use(cors());
 app.use(express.json());
-app.get('/',(req,res)=>res.send('BLOGHUB API Running'));
-app.listen(5000,()=>console.log('Server running'));
+
+app.use("/api/admin", adminRoutes);
+app.use("/api/blog", blogRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
